d4bae9fa15b5e36b90173357a7537632af809fae
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-07-22 09:29:07 -0700

    props properties

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1 ref="greet">Hi</h1>
+  <h1 ref="greet">Vue3 Js</h1>
   <input type="text" ref="name"/>
   <button @click="clickHandler">Click</button>
-  <p>Mornee</p>
+  <div class="btn">
+    <button @click="successClick">Success</button>
+    <button @click="deleteClick">Delete</button>
+  </div>
+
 
   <!--3.Use -->
-  <Modal></Modal>
+  <Modal class="hide" :header="success.header" :content="success.content" :theme="success.theme" id="success"></Modal>
+  <Modal class="hide" :header="danger.header" :content="danger.content" :theme="danger.theme" id="danger"></Modal>
   <!-- <Modal/> -->
 </template>
 
@@ -16,7 +21,16 @@ import Modal from "./components/Modal.vue"
 export default {
     data(){
       return {
-        title:"You don't know js"
+            success:{
+                header:"Login Success",
+                content:"Welcome From Our App",
+                theme:"success"
+            },
+            danger:{
+                header:"Delete Process",
+                content:"Are you sure to delete it ?",
+                theme:"delete"
+            }
       }
     },
     methods:{
@@ -27,7 +41,24 @@ export default {
           console.log(this.$refs.name);
           this.$refs.name.focus();
           this.$refs.greet.textContent="Greeting";
-      }
+      },
+  
+      successClick(){
+           const success=document.getElementById('success');
+           const danger=document.getElementById('danger');
+           console.log(success);
+          
+           danger.classList.remove('show');
+           success.classList.add('show');
+      },
+      deleteClick(){
+           const success=document.getElementById('success');
+           const danger=document.getElementById('danger');
+           console.log(danger);
+           success.classList.remove('show');
+           danger.classList.add('show');
+      },
+     
     },
     components:{
       //2.Register
@@ -42,8 +73,36 @@ export default {
   width: 100vh;
   height: 100vh;
 }
-
 h1{
   color:blue;
+}
+
+.hide{
+  display: none;
+}
+.show{
+  display: block;
+}
+.btn{
+  padding:20px;
+}
+.btn button{
+  width:80px;
+  height:50px;
+  color:#fff;
+  font-size: 16px;
+ 
+  border:0;
+  margin:5px;
+  border-radius: 4px;
+}
+.btn button:focus{
+  transform: scale(0.899);
+}
+button:nth-child(1){
+  background-color: green;
+}
+button:nth-child(2){
+  background-color: crimson;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="backdrop">
-      <div class="modal">
+      <div class="modal" :class="{success:theme==='success',danger:theme==='delete'}">
         <h1>Modal Title</h1>
           <p>Modal content</p>
+          <span>{{header}}</span>
+          <span>{{content}}</span>
       </div>
   </div>
  
@@ -10,16 +12,20 @@
 
 <script>
 export default {
-
+      props:['header','content','theme']
 }
 </script>
 
 <style scoped>
 
   .backdrop{
-      background-color: rgba(0,0,0,0.5);
-      width:100%;
-      height:100%;
+      /* background-color: rgba(0,0,0,0.5); */
+      width:500px;
+      height:500px;
+      background-color: transparent;
+      margin:200px 50px 0px 50px;
+
+      
 
       display: flex;
       justify-content: center;
@@ -33,11 +39,22 @@ export default {
       width:400px;
       padding:10px;
       border:5px solid palegreen;
+      box-shadow: 0px 10px 10px rgba(0,0,0,0.5);
+      
       border-radius: 10px;
-      background-color: #fff;
+      background-color: rgb(21, 189, 167);
   }
   p{
     font-style: normal;
   }
+  .success{
+    background-color: green;
+    color:#fff;
+  }
+  .danger{
+    background-color: crimson;
+    color:#fff;
+  }
+
   
 </style>
